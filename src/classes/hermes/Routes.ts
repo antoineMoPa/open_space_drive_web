060a0cf3953f4ccf697b03308aa4b9e94b1f8edc
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2021-11-21 18:31:12 -0500

    Find road segment up/right/forward vectors.

--- src/classes/hermes/Routes.ts
+++ src/classes/hermes/Routes.ts
@@ -10,12 +10,55 @@ export default class Routes {
         this.update();
     }
 
-    buildRoadSegment({x1,y1,z1,x2,y2,z2,upX,upY,upZ}): BABYLON.VertexData {
+    buildRoadSegmentAndUpVectorLines({up, p1, forward, right}): BABYLON.Mesh[] {
+        const scene = this.hermes.app.scene;
+        const lineMesh = new BABYLON.Mesh("road", scene);
+        const lineMaterial = new BABYLON.StandardMaterial("road", scene);
+        lineMaterial.wireframe = true;
+        lineMesh.material = lineMaterial;
+
         const vertexData = new BABYLON.VertexData();
-        vertexData.positions = [x1, y1, z1, x2, y2, z2,
-                                x1, y1, z1, x1 + upX, y1 + upY, z1 + upZ];
-        vertexData.indices = [0, 1, 2, 3];
-        return vertexData;
+        vertexData.positions = [p1.x, p1.y, p1.z,
+                                p1.x + forward.x, p1.y + forward.y, p1.z + forward.z,
+                                p1.x, p1.y, p1.z,
+                                p1.x + up.x, p1.y + up.y, p1.z + up.z,
+                                p1.x, p1.y, p1.z,
+                                p1.x + right.x, p1.y + right.y, p1.z + right.z];
+        vertexData.indices = [0, 1, 2, 3, 4, 5];
+        vertexData.applyToMesh(lineMesh);
+
+        return [lineMesh];
+    }
+
+    buildRoadSegment({up, p1, forward, right}): BABYLON.Mesh[] {
+        const scene = this.hermes.app.scene;
+        const lineMesh = new BABYLON.Mesh("road", scene);
+        const lineMaterial = new BABYLON.StandardMaterial("road", scene);
+        lineMaterial.wireframe = true;
+        lineMesh.material = lineMaterial;
+
+
+        //    _                  ^ up               _
+        //   | |                 |                 | |
+        //   |_|___________________________________|_|
+        //   |                   . p1  ----> right   |
+        //   |_______________________________________|
+        //
+        //    TODO, implement the shape above, both blocks at the side removable with an option
+        //    (this will facilitate makin highway exits)
+
+
+        const vertexData = new BABYLON.VertexData();
+        vertexData.positions = [p1.x, p1.y, p1.z,
+                                p1.x + forward.x, p1.y + forward.y, p1.z + forward.z,
+                                p1.x, p1.y, p1.z,
+                                p1.x + up.x, p1.y + up.y, p1.z + up.z,
+                                p1.x, p1.y, p1.z,
+                                p1.x + right.x, p1.y + right.y, p1.z + right.z];
+        vertexData.indices = [0, 1, 2, 3, 4, 5];
+        vertexData.applyToMesh(lineMesh);
+
+        return [lineMesh];
     }
 
     deleteAll() {
@@ -32,8 +75,6 @@ export default class Routes {
         this.customMeshes.forEach(mesh => mesh.dispose());
         this.customMeshes = [];
 
-        const roadMaterial = new BABYLON.StandardMaterial("road", scene);
-        roadMaterial.wireframe = true;
         let offset = 0;
 
         if (!results[0]) {
@@ -41,15 +82,20 @@ export default class Routes {
         }
 
         results[0].values.forEach((row) => {
-            const customMesh = new BABYLON.Mesh("road", scene);
             const positions = [];
             const indices = [];
-            customMesh.material = roadMaterial;
 
             const [x1,y1,z1,x2,y2,z2,upX,upY,upZ] = row;
-            const vertexData = this.buildRoadSegment({x1,y1,z1,x2,y2,z2,upX,upY,upZ});
-            vertexData.applyToMesh(customMesh);
-            this.customMeshes.push(customMesh);
+            const up = new BABYLON.Vector3(upX, upY, upZ);
+            const p1 = new BABYLON.Vector3(x1, y1, z1);
+            const p2 = new BABYLON.Vector3(x2, y2, z2);
+            const forward = p2.subtract(p1);
+            const right = up.cross(forward).normalize();
+
+            const mesh = this.buildRoadSegment({up, p1, forward, right});
+            const lines = this.buildRoadSegmentAndUpVectorLines({up, p1, forward, right});
+
+            this.customMeshes.push(...mesh, ...mesh);
         });
     };
 
