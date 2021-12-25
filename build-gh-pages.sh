4c5a97f21eaf5c9227d420206672f4fe9397a391
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2021-12-25 01:44:29 -0500

    Add more sqlite deps.

--- build-gh-pages.sh
+++ build-gh-pages.sh
@@ -12,7 +12,7 @@ git branch -D gh-pages &&\
     git add .nojekyll &&\
     git add -f dist/* &&\
     git add -f node_modules/ammo.js/builds/ammo.js &&\
-    git add -f node_modules/sql.js/dist/sql-wasm.js &&\
+    git add -f node_modules/sql.js/dist/sql-wasm.* &&\
     git mv -f dist/* . && \
     git commit -m "deploy";
 
